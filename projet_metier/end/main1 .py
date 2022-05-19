f54624480949758c31b926bab52c1179de8caa9c
Diaffat <fatoumatasoukouradiassana18@gmail.com>
2022-05-19 15:43:35 +0100

    ypred

--- projet_metier/end/main1 .py
+++ projet_metier/end/main1 .py
@@ -133,7 +133,7 @@ def principal():
      data = pd.read_csv("projet_metier\end\Classeur11.csv") 
      x = data.iloc[:,0:9]
      y= data.iloc[:,10]
-     choose = option_menu("Main Menu",["Home","Dataset","Map","Regression","Prediction","About"],
+     choose = option_menu("Main Menu",["Home","Dataset","Map","Regression","Modeles Training","Prediction"],
     icons=['house','file','pin-map','graph-up','pie-chart-fill','person lines fill'],
      menu_icon = "list", default_index=0,
     styles={
@@ -304,30 +304,20 @@ def principal():
            plt.ylabel("DBO5")
            st.pyplot(fig)
   #*********************************************Prediction**************************************************************
-     elif choose == "Prediction":
+     elif choose == "Modeles Training":
          loti("projet_metier\end\98831-pie-chart.json")
          st.title("Prediction")
          coll1, coll2= st.columns(2)
-         kind = coll1.selectbox("Kind of Predict",["One Predict","Whith File"])
+         kind = coll1.selectbox("Kind of Predict",["One Training","Whith File"])
          model = coll2.selectbox("Model",["SVC","Tree","KNN","Voting","Bagging","Perceptron","MLPClassifier","RidgeClassifier","GMM","QuadraticDiscriminantAnalysis","OneVsOneClassifier","GaussianMixture","AdaBoostClassifier"])
         
     
     
     
-         if kind =="One Predict":
-            st.header(" Saisir les donner d'entrées")
-            PH = st.number_input("PH")
-            T = st.number_input("T")
-            CE = st.number_input("CE")
-            O2 = st.number_input("O2")
-            NH = st.number_input("NH")
-            NO = st.number_input("NO")
-            SO = st.number_input("SO")
-            PO = st.number_input("PO")
-            DBO5 = st.number_input("DBO5")
-            val = [PH, T, CE,	O2,	NH,	NO,	SO,	PO,	DBO5]
-        
+         if kind =="One Training":
+            st.balloons()
          elif kind == "Whith File":
+              st.balloons()
               st.subheader("Import Dataset")
               datafile = st.file_uploader("Upload CSV",type=["CSV"])
               if datafile is not None:
@@ -673,7 +663,6 @@ def principal():
                    print(m.__class__.__name__ , m.score(x_test, y_test))
 #**************************gridsearch********************************************************                         
          if gridsearch:
-
            if model == "SVC":
              params = {"C":np.arange(Min_value_C,Max_value_C,0.1),"gamma":np.arange(Min_value_g,Max_value_g,0.1),"kernel":kernel}
              grid = GridSearchCV(svc,params)
@@ -736,7 +725,7 @@ def principal():
              llp = grid.best_estimator_
            if model == "GMM":
              params = {'n_components': np.arange(gml, gmll),'covariance_type': [gm],'n_iter': np.arange(gmlp22, gmlp23)}
-             grid = GridSearchCV(gmm, params)
+             grid =GridSearchCV(gmm, params)
              grid.fit(x_train,y_train)
              grid.best_params_
              gmm= grid.best_estimator_
@@ -790,6 +779,7 @@ def principal():
              grid.best_params_
              gau= grid.best_estimator_
             if model == "OneVsOneClassifier":
+         
              params = {'estimator': [oee], 'n_jobs': np.arange(one1, one2)}
              grid = RandomizedSearchCV(one, params)
              grid.fit(x_train,y_train)
@@ -930,43 +920,7 @@ def principal():
                 plot_precision_recall_curve(svc, x_test, y_test)
                 st.pyplot()
 # **************predict***************************************************
-
-
-
-           ypred = model.predict([val])
-           if st.button("Predict"):
-              #val = [PH, T, CE,	O2,	NH,	NO,	SO,	PO,	DBO5]
-             if model =="SVC":
-                  ypred = svc.predict([val])
-
-                  if ypred[0]==1:
-                   classe = "Excellente"
-                  if ypred[0]==2:
-                   classe = "Bonne"
-                  if ypred[0]==3:
-                   classe = "Peu Polluée"
-                  if ypred[0]==4:
-                   classe = "Mauvaise"
-                  if ypred[0]==5:
-                   classe = "Très mauvaise"
-                  st.success(f"La prediction est: {classe}")
-             if model =="KNN":
-                  ypred = knn.predict([val])
-
-                  if ypred[0]==1:
-                   classe = "Excellente"
-                  if ypred[0]==2:
-                   classe = "Bonne"
-                  if ypred[0]==3:
-                   classe = "Peu Polluée"
-                  if ypred[0]==4:
-                   classe = "Mauvaise"
-                  if ypred[0]==5:
-                   classe = "Très mauvaise"
-                  st.success(f"La prediction est: {classe}")
-           
-
-
+      
       # **************Map***************************************************
      elif choose =="Map":
            city = pd.DataFrame ( { 'awesome cities' : ["Ait boulmane", "Source Arbalou" ,"Boumia", "Zaïda","AnzarOufounas", "Tamdafelt", "Missour",  "Sebra","Safsaf", "Pont Hassan II","Moulouya"],
@@ -986,7 +940,197 @@ def principal():
                          "lat": [506388.899, 508378.875, 510270.048, 521267.473, 528157.842, 541119.868, 522710.665, 523517.135, 529513.51, 545371.311, 608563.842, 632589.028, 657696.741, 667765.779, 688527.58, 691373.449, 716153.504, 717261.981, 750073.689, 752451.688, 770940.249, 774425.807],
                          "lon": [ 223813.71, 225419.701, 231109.507, 239017.267, 235764.271, 246884.595, 203771.489, 209213.863, 213616.77, 238557.759, 254198.713, 273722.851, 304862.154, 340891.631, 403762.245, 406608.844, 442463.459, 442636.19, 479499.227, 481921.171, 498404.754, 503613.795]})
         st.map(site)
+     elif choose=="Prediction":
+            st.title("Prediction")
+            coll1, coll2= st.columns(2)
+            kind = coll1.selectbox("Predict",["One Prediction"])
+            model = coll2.selectbox("Model",["SVC","Tree","KNN","Voting","Bagging","Perceptron","MLPClassifier","RidgeClassifier","GMM","QuadraticDiscriminantAnalysis","OneVsOneClassifier","GaussianMixture","AdaBoostClassifier"])
+            st.header(" Saisir les donner d'entrées")
+            PH = st.number_input("PH")
+            T = st.number_input("T")
+            CE = st.number_input("CE")
+            O2 = st.number_input("O2")
+            NH = st.number_input("NH")
+            NO = st.number_input("NO")
+            SO = st.number_input("SO")
+            PO = st.number_input("PO")
+            DBO5 = st.number_input("DBO5")
+            val = [PH, T, CE,	O2,	NH,	NO,	SO,	PO,	DBO5]
+            if st.button("Predict"):
+              #val = [PH, T, CE,	O2,	NH,	NO,	SO,	PO,	DBO5]
+             if model =="SVC":
+               params = {"C":np.arange(Min_value_C,Max_value_C,0.1),"gamma":np.arange(Min_value_g,Max_value_g,0.1),"kernel":kernel}
+               grid = RandomizedSearchCV(svc,params)
+               grid.fit(x_train,y_train)
+               grid.best_params_
+               svc = grid.best_estimator_
+               ypred = svc.predict([val])
 
+               if ypred[0]==1:
+                  classe = "Excellente"
+               if ypred[0]==2:
+                classe = "Bonne"
+               if ypred[0]==3:
+                  classe = "Peu Polluée"
+               if ypred[0]==4:
+                  classe = "Mauvaise"
+               if ypred[0]==5:
+                  classe = "Très mauvaise"
+               st.success(f"La prediction est: {classe}")
+             if model =="KNN":
+               ypred = knn.predict([val])
+
+               if ypred[0]==1:
+                  classe = "Excellente"
+               if ypred[0]==2:
+                  classe = "Bonne"
+               if ypred[0]==3:
+                  classe = "Peu Polluée"
+               if ypred[0]==4:
+                  classe = "Mauvaise"
+               if ypred[0]==5:
+                  classe = "Très mauvaise"
+               st.success(f"La prediction est: {classe}")
+             if model =="Tree":
+               ypred =mtree.predict([val])
+
+               if ypred[0]==1:
+                  classe = "Excellente"
+               if ypred[0]==2:
+                  classe = "Bonne"
+               if ypred[0]==3:
+                  classe = "Peu Polluée"
+               if ypred[0]==4:
+                  classe = "Mauvaise"
+               if ypred[0]==5:
+                  classe = "Très mauvaise"
+               st.success(f"La prediction est: {classe}")
+             if model =="Perceptron":
+               ypred =percep.predict([val])
+
+               if ypred[0]==1:
+                  classe = "Excellente"
+               if ypred[0]==2:
+                  classe = "Bonne"
+               if ypred[0]==3:
+                  classe = "Peu Polluée"
+               if ypred[0]==4:
+                  classe = "Mauvaise"
+               if ypred[0]==5:
+                  classe = "Très mauvaise"
+               st.success(f"La prediction est: {classe}")
+             if model =="MLPClassifier":
+               ypred = mlp.predict([val])
+
+               if ypred[0]==1:
+                  classe = "Excellente"
+               if ypred[0]==2:
+                  classe = "Bonne"
+               if ypred[0]==3:
+                  classe = "Peu Polluée"
+               if ypred[0]==4:
+                  classe = "Mauvaise"
+               if ypred[0]==5:
+                  classe = "Très mauvaise"
+               st.success(f"La prediction est: {classe}")
+             if model =="RidgeClassifier":
+               ypred = ridge.predict([val])
+
+               if ypred[0]==1:
+                  classe = "Excellente"
+               if ypred[0]==2:
+                  classe = "Bonne"
+               if ypred[0]==3:
+                  classe = "Peu Polluée"
+               if ypred[0]==4:
+                  classe = "Mauvaise"
+               if ypred[0]==5:
+                  classe = "Très mauvaise"
+               st.success(f"La prediction est: {classe}")
+             if model =="AdaBoostClassifier":
+               ypred = ada.predict([val])
+
+               if ypred[0]==1:
+                  classe = "Excellente"
+               if ypred[0]==2:
+                  classe = "Bonne"
+               if ypred[0]==3:
+                  classe = "Peu Polluée"
+               if ypred[0]==4:
+                  classe = "Mauvaise"
+               if ypred[0]==5:
+                  classe = "Très mauvaise"
+               st.success(f"La prediction est: {classe}")
+             if model =="QuadraticDiscriminatAnalysis":
+               ypred = qua.predict([val])
+
+               if ypred[0]==1:
+                  classe = "Excellente"
+               if ypred[0]==2:
+                  classe = "Bonne"
+               if ypred[0]==3:
+                  classe = "Peu Polluée"
+               if ypred[0]==4:
+                  classe = "Mauvaise"
+               if ypred[0]==5:
+                  classe = "Très mauvaise"
+               st.success(f"La prediction est: {classe}")
+             if model =="GausianMixture":
+               ypred = gau.predict([val])
+
+               if ypred[0]==1:
+                  classe = "Excellente"
+               if ypred[0]==2:
+                  classe = "Bonne"
+               if ypred[0]==3:
+                  classe = "Peu Polluée"
+               if ypred[0]==4:
+                  classe = "Mauvaise"
+               if ypred[0]==5:
+                  classe = "Très mauvaise"
+               st.success(f"La prediction est: {classe}")
+             if model =="OneVsOneClassifier":
+               ypred = one.predict([val])
+
+               if ypred[0]==1:
+                  classe = "Excellente"
+               if ypred[0]==2:
+                  classe = "Bonne"
+               if ypred[0]==3:
+                  classe = "Peu Polluée"
+               if ypred[0]==4:
+                  classe = "Mauvaise"
+               if ypred[0]==5:
+                  classe = "Très mauvaise"
+               st.success(f"La prediction est: {classe}")
+             if model =="LabelPropagation":
+               ypred = llp.predict([val])
+
+               if ypred[0]==1:
+                  classe = "Excellente"
+               if ypred[0]==2:
+                  classe = "Bonne"
+               if ypred[0]==3:
+                  classe = "Peu Polluée"
+               if ypred[0]==4:
+                  classe = "Mauvaise"
+               if ypred[0]==5:
+                  classe = "Très mauvaise"
+               st.success(f"La prediction est: {classe}")
+             if model =="GMM":
+               ypred = gmm.predict([val])
+
+               if ypred[0]==1:
+                  classe = "Excellente"
+               if ypred[0]==2:
+                  classe = "Bonne"
+               if ypred[0]==3:
+                  classe = "Peu Polluée"
+               if ypred[0]==4:
+                  classe = "Mauvaise"
+               if ypred[0]==5:
+                  classe = "Très mauvaise"
+               st.success(f"La prediction est: {classe}")
 
 principal()
 
